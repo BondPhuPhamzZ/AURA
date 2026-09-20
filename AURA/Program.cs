7caf382d7887f148dc998747b170a641c330610b
BondSensei <phuphamwork89@gmail.com>
2026-09-20 13:25:38 +0700

    build: Setup SQL Server ConnectionString and register DbContext in Program.cs

--- AURA/Program.cs
+++ AURA/Program.cs
@@ -2,16 +2,23 @@
 using AURA.Services;
 using AURA.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Setup Entity Framework Core SQL Server
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Register AURA DI Services
-builder.Services.AddSingleton<IReimbursementRepository, MockReimbursementRepository>();
+builder.Services.AddScoped<IReimbursementRepository, ReimbursementRepository>();
+builder.Services.AddScoped<IAuditLogger, AuditLogger>();
 builder.Services.AddScoped<IVisualValidator, Gpt4oValidatorService>();
 
 var app = builder.Build();
